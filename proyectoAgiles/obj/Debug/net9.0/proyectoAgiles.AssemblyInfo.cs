4a4033ed1ab5b10b21f2e063ec18f62dbb89e471
NIXONHS <nixon2000paul@gmail.com>
2025-06-03 17:34:48 -0500

    Correciiones menores

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3913c82b3a7015a30ede5c5f0678f29cf606ca1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d15c735715ec8619c14c2f877861d692f54b5100")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
