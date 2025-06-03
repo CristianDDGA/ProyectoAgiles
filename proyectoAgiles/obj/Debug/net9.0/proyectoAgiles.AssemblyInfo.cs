fccc9af4621b8e68159139af4a54e8b9ec73c6a0
NIXONHS <nixon2000paul@gmail.com>
2025-06-03 01:37:40 -0500

    Se implemnto interfaces

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3168f9179a6dc4cb312b6df8926fe60370473b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44ea0cb938ff28b81f1c4731c56a23105e10794")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
