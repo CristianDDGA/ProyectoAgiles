e953eb24d6df711d7adfaf7296296cc1dfe1841a
NIXONHS <nixon2000paul@gmail.com>
2025-06-04 18:57:06 -0500

    Se corrigio el drop de carga de imagen

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0faeb3f9d96b526ce113c257e126f6975370deb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e461aed894246bd00233cdde47416925eb4ae1ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
