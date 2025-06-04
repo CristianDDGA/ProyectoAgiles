e0faeb3f9d96b526ce113c257e126f6975370deb
NIXONHS <nixon2000paul@gmail.com>
2025-06-04 18:30:41 -0500

    Mejorar la interfaz carrousel

--- ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
+++ ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c139afb336d9cfe835d5a45d71793fc8a405381a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddb5c4fc4be3a71635670f5a1abaa4b9f80be3da")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
