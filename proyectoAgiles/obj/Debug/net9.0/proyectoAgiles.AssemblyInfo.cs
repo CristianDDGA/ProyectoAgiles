0c4eff08bedbce8cbcfdef6fda19aaae0909c7a1
NIXONHS <nixon2000paul@gmail.com>
2025-06-11 15:53:06 -0500

    Correcciones menores en las interfaces admin

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0c47659f0e812f0dd533ea47780d2106a10fd11")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f619b8673b29d3146100026d235780c73e69e392")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
