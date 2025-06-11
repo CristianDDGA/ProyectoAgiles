47db4e1ffdaa1fa3d4a68b5c5a6b6f1f182eb250
NIXONHS <nixon2000paul@gmail.com>
2025-06-11 12:17:43 -0500

    Arreglos menores en recuperar contrasena

--- ProyectoAgiles.Infrastructure/obj/Debug/net9.0/ProyectoAgiles.Infrastructure.AssemblyInfo.cs
+++ ProyectoAgiles.Infrastructure/obj/Debug/net9.0/ProyectoAgiles.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb3ca3e8198071737477d73614e5b3e6a26ad62e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e627b857e08667dbb3d6cb2f486c0a8d34d1d098")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f7b1f4c3a70e996c3d1e55ec6003e0bec58548e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e627b857e08667dbb3d6cb2f486c0a8d34d1d098")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
