12a28baa476c72526a05a0c3af04d94ad4ec7677
NIXONHS <nixon2000paul@gmail.com>
2025-06-11 17:27:06 -0500

    Corregido base de datos correct

--- ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
+++ ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9226b1964ad0308656f83298e1f8b77df91fdcbc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5612863c48c0b3c9967fa3710ffe66922f4c8b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
