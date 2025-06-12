aae34f25f63e597f8ba3351b9e8cbb3be1bb5cee
NIXONHS <nixon2000paul@gmail.com>
2025-06-12 14:05:51 -0500

    Agregado Sprint 1

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3dcdb3c154684daefaec60bb5ca0b0a5a66208f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348f205d1a6098283d19d7ea84aeca44a3169c5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
