b7c39dc9f3ef44824b6515d96f979c49fd7d82f9
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 14:39:59 -0500

    Agregada targeta de admin boton

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6ee7ab69194a26adfc2bcfcb50d1fe218e52bce")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b7d711ef7d2f0cc29e77343b7e59c7b6abba058")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
