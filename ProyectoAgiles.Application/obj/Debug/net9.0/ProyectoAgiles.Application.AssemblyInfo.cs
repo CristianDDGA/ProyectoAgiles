04d6c02dbdf474b11ad1262ce65436a0dfa9f22c
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 18:28:27 -0500

    Se corrigio errores en docentes y admin

--- ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
+++ ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af18838bdd41917b82d0b1047a4e2561fa27a102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d80e1d7facad3901d45e6837bd678a9d87ba64")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af18838bdd41917b82d0b1047a4e2561fa27a102")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28d80e1d7facad3901d45e6837bd678a9d87ba64")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
