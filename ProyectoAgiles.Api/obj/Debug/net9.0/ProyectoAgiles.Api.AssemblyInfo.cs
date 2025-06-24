af18838bdd41917b82d0b1047a4e2561fa27a102
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 18:22:04 -0500

    Se corrigio errores en docentee

--- ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
+++ ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db5c91fbf492d0ba2ac905fc62f807fb73e4e21d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c85c0a77419d20ec1663e1564281bb2f0efcf3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
