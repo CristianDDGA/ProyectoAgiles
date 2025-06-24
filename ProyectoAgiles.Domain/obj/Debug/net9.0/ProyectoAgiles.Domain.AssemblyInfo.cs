a07d5bf8f03f5d880c854e47abac218825850cca
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 14:05:46 -0500

    Agregada targeta de TTHH

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51850183cc70bbb4b475d27e99ab01d4d3bc5c89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26c163469a7d8a010b6e7b08c7f55c4afb1b6296")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
