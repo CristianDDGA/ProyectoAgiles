b4b578fc3674487dc0b0c45d7a70da5a99d0b88b
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 17:24:50 -0500

    Se corrigio errores en solicitudes en formulario admin

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ab071fa797d15131f647f9e6876007f1acc177d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61ef69a40de89f0a559f60c267d8de037b3ca0e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
