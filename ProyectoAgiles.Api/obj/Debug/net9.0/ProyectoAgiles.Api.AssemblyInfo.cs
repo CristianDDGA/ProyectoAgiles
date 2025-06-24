d9debc64787be4a6da3f186e1f4f2d8d1297e919
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 00:06:18 -0500

    Correcciones

--- ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
+++ ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffc8ca6a0bb30211e16854e5f95f1dd1a22ff3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5152d7bc72bc609aab4edb02ee04fbeea1164ba8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
