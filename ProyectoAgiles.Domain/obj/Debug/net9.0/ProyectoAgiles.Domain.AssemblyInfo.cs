0c6d6f8fe23cce3f2bf7cad8b6c1538a9f810045
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 00:56:08 -0500

    Correcciones menores

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9851608af756492cfc29fa037e5cc756f5982b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7627e9ef14f83c8aa348e57bc63e6a709a3e3496")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
