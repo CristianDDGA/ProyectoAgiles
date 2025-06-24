4007ff5e03aa8c487171c4a43baf46ec47e3869b
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 09:37:08 -0500

    Correcciones en las tablas corecc

--- ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
+++ ProyectoAgiles.Application/obj/Debug/net9.0/ProyectoAgiles.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669916e716255a6eef1477a6c89c6bda27da36ed")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88960f903523acdebe18036a8b191a184e354d04")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
