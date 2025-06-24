255d60f80b25214a556c87886d2dee816a01b4de
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 14:19:12 -0500

    Agregada targeta de admin

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+26c163469a7d8a010b6e7b08c7f55c4afb1b6296")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07d5bf8f03f5d880c854e47abac218825850cca")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
