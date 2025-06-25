304795ddd25c238b76bf8fa580af62abbc9453a0
NIXONHS <nixon2000paul@gmail.com>
2025-06-25 00:15:38 -0500

    Se implemento Direccion

--- ProyectoAgiles.Infrastructure/obj/Debug/net9.0/ProyectoAgiles.Infrastructure.AssemblyInfo.cs
+++ ProyectoAgiles.Infrastructure/obj/Debug/net9.0/ProyectoAgiles.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e29d76d5e9977d5b924f7478506f0c739205a62d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8e943bb1763a5b0832cfbd28019738c94d80bb2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
