909577f092e872c95611329baa3bbddadb85874a
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 21:58:55 -0500

    Correccion en la zona del pdf

--- ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
+++ ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a86cae30756a80a77c377e6c952d317a05a7567")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ccda7546ea7d03a8f90dfd62c4acadb9077d70f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
