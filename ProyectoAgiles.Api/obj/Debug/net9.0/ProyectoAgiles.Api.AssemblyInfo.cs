33350e44b44b9e6fbf47dff84dbb5a18d9041caf
NIXONHS <nixon2000paul@gmail.com>
2025-06-25 11:51:55 -0500

    Se implemento mejoras docente

--- ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
+++ ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d57b170025b4ad284fe4af5e49a0754d8a39b3f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c4b23895b1347cd26de8b6fe5997a216a629f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77899923b7e87cf83ce17ff84f81602abbef8ea5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2c4b23895b1347cd26de8b6fe5997a216a629f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
