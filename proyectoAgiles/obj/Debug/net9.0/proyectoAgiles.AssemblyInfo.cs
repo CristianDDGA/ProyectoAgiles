e29d76d5e9977d5b924f7478506f0c739205a62d
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 23:41:29 -0500

    Se implemento Direccion escalafon corregido

--- proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
+++ proyectoAgiles/obj/Debug/net9.0/proyectoAgiles.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+767a57b387a225eb10cee8da565df7d0f2e810e5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dc4430e3d785706633be859c2f55527cec0059f")]
 [assembly: System.Reflection.AssemblyProductAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("proyectoAgiles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
