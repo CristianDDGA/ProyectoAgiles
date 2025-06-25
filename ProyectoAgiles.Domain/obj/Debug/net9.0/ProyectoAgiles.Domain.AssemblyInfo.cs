f9ed15bdcfe83870dc4891164de8689b1bacaa8f
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 20:09:51 -0500

    Todo corregido admin

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89279cae465dbc1903a2c5ec3ee3351ffc525b02")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a66a8b1a78d4180a7b6d5a034907b743745157")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
