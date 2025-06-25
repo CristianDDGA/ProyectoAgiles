3c20432eb6798cbd01f81149f4e81a5a627476ea
NIXONHS <nixon2000paul@gmail.com>
2025-06-24 21:34:29 -0500

    Se agregaron mejoras en los formularios

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85025bc3638d8080d9d9fd46300c43f00cc42456")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccfdd5e80a1c422e00e89480ec4b5cca2c1499f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
