06de5efd9a4695d70f9ede6210d96201f7ef973f
NIXONHS <nixon2000paul@gmail.com>
2025-06-26 01:52:21 -0500

    Se implemento mejoras en docentes

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1dd6a469ef1c3e3316c9e64d075c26082f1474a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bef9018bd2a0a1fa1adf8172fd07bd1b67562c56")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
