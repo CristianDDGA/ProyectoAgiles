1dd6a469ef1c3e3316c9e64d075c26082f1474a1
NIXONHS <nixon2000paul@gmail.com>
2025-06-26 01:07:02 -0500

    Se implemento mejoras se elimino locales en docentes

--- ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
+++ ProyectoAgiles.Domain/obj/Debug/net9.0/ProyectoAgiles.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7baa79c1f2db78b9a51442ad3a61f8c74e4327d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20a39ddb60087b254e0d533f7667a52194096d61")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
