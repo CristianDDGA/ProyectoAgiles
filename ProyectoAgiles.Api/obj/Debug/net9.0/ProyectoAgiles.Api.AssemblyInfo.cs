ae576dac701133f0ff4b869971c8ac1016209a0f
NIXONHS <nixon2000paul@gmail.com>
2025-07-03 01:35:39 -0500

    Se mejoro historial docentes

--- ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
+++ ProyectoAgiles.Api/obj/Debug/net9.0/ProyectoAgiles.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343a4e4f09861959465f81b1a168930faf4eb12a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bec65f79c6790da9b6efaac9b915098e529a453e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoAgiles.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
